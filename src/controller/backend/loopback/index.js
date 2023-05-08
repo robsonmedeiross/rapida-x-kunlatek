4c343572a68dc6137b5cd59b82479eae3919e99b
Victor Machado <joelvictor2012@gmail.com>
2023-05-08 09:57:23 -0300

    Autocomplete base

--- src/controller/backend/loopback/index.js
+++ src/controller/backend/loopback/index.js
@@ -1,7 +1,7 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 const {
-  createArrayOverFolderFiles
+  array: { createArrayOverFolderFiles },
 } = require("kunla-utils");
 const projectsPath = path.join(__dirname, "..", "..", "..", "..", "project");
 // const {createCodeOverElement} = require("./form/index");
@@ -11,43 +11,46 @@ const startLoopbackCoding = async (project) => {
     `${projectsPath}/${project.folder}`
   );
   await takeObject(project, filesInProjectFolderToSetParams);
-}
+};
 
 takeObject = (project, filesInProjectFolderToSetParams) => {
   filesInProjectFolderToSetParams.forEach(async (file) => {
-    if (file != '') {
-      const string = fs.readFileSync(`${projectsPath}/${project.folder}/${file}`, "utf8");
+    if (file != "") {
+      const string = fs.readFileSync(
+        `${projectsPath}/${project.folder}/${file}`,
+        "utf8"
+      );
       const object = JSON.parse(string);
       await takeElements(project, object);
     }
   });
-}
+};
 
 takeElements = async (project, object) => {
-  if (object.kind !== 'form') {
+  if (object.kind !== "form") {
     console.info("Only forms set here");
     return ``;
   }
-  
+
   for (const key in object) {
     if (Object.hasOwnProperty.call(object, key)) {
       if (key === "elements") {
         const elements = object[key];
-        elements.forEach(element => {
-          console.log(element)
+        elements.forEach((element) => {
+          console.log(element);
           // createCodeOverElement(project,element)
         });
       }
     }
   }
-}
+};
 
 startLoopbackCoding({
   folder: "animation",
   title: "Animação",
-  ui: "material"
-})
+  ui: "material",
+});
 
 module.exports = {
-  startLoopbackCoding
-}
+  startLoopbackCoding,
+};
--- src/controller/frontend/angular/form/material/template/autocomplete.js
+++ src/controller/frontend/angular/form/material/template/autocomplete.js
@@ -1,7 +1,39 @@
+const uuidv4 = require("uuid");
+
+const defaultAttributes = {
+  id: uuidv4(), // Defaulting to uuid for now
+  options: [],
+  conditions: "",
+  label: "",
+  isAsync: false,
+};
+
+const componentBaseCode = (attributes = defaultAttributes) => `
+  <mat-autocomplete #${attributes.id}="matAutocomplete" ${
+  attributes.conditions
+}>
+    <mat-label>${attributes.label}</mat-label>
+
+    <input type="text"
+        placeholder="${attributes.placeholder}"
+        matInput
+        [formControl]="${attributes.name}"
+        [matAutocomplete]="${attributes.id}">
+
+    <mat-option *ngFor="let option of ${attributes.id}Options${
+  attributes.isAsync ? " | async" : ""
+}" [value]="option.value">
+      {{option.name}}
+    </mat-option>
+  </mat-autocomplete>
+`;
+
 const createAutocompleteCode = async (project, element) => {
-  
-}
+  let code = componentBaseCode(element);
+
+  return code;
+};
 
 module.exports = {
-  createAutocompleteCode
-}
+  createAutocompleteCode,
+};
